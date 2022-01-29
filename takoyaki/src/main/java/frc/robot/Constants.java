ed658e41045ac7416dd4774551d055653c12e83b
ary <aryan@jha.io>
2022-01-28 16:35:37 -0800

    fix duplicate class in constants

--- takoyaki/src/main/java/frc/robot/Constants.java
+++ takoyaki/src/main/java/frc/robot/Constants.java
@@ -7,6 +7,12 @@ package frc.robot;
 import edu.wpi.first.wpilibj.XboxController;
 
 public final class Constants {
+  
+  public static final class kOI {
+    public static final int DRIVE_CONTROLLER = 0;
+    public static final int OPERATOR_CONTROLLER = 1;
+  }
+  
   public static final class kIntake {
     public static final int FALCON_MOTOR_ID = -1;
     public static final int SOLENOID_FRONT = 1;
@@ -19,7 +25,6 @@ public final class Constants {
     public static final int RUN_INTAKE = XboxController.Button.kX.value;
     public static final int REVERSE_INTAKE = XboxController.Button.kB.value;
 
-public final class Constants {
   public static final class kDrive {
     public static final int FRONT_RIGHT_ID = 3;
     public static final int FRONT_LEFT_ID = 1;
@@ -27,8 +32,4 @@ public final class Constants {
     public static final int BACK_LEFT_ID = 2;
   }
 
-  public static final class kOI {
-    public static final int DRIVE_CONTROLLER = 0;
-    public static final int OPERATOR_CONTROLLER = 1;
-  }
 }
