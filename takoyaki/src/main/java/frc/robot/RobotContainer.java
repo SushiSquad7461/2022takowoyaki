ca2360610a01e64668f0113183567f8e2fe4993c
poroheart <52479973+poroheart@users.noreply.github.com>
2022-02-04 18:40:32 -0800

    Update constants and robotcontainer temp (#19)
    * moves button values to kOI and changed to fit drive controller layout
    
    * updated joystickbutton values and added shooter commands
    
    * updated hopper to keep naming consistent
    
    * updated intake to toggle actuation instead of one to actuate and one to restract
    
    Co-authored-by: Porohart <56803529+Porohart@users.noreply.github.com>

--- takoyaki/src/main/java/frc/robot/RobotContainer.java
+++ takoyaki/src/main/java/frc/robot/RobotContainer.java
@@ -8,10 +8,12 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.subsystems.Hopper;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RobotContainer {
@@ -19,6 +21,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final Hopper hopper = new Hopper();
   private final Intake intake = new Intake();
+  private final Shooter shooter = new Shooter();
   private final Drivetrain drivetrain = new Drivetrain();
   
   // Controllers
@@ -39,32 +42,45 @@ public class RobotContainer {
    */
   private void configureButtonBindings() {
     // run hopper
-    new JoystickButton(driveController, Constants.kHopper.RUN_HOPPER)
-      .whenPressed(new InstantCommand(hopper::runForward, hopper))
-      .whenReleased(new InstantCommand(hopper::stop, hopper));
+    new JoystickButton(driveController, Constants.kOI.RUN_HOPPER)
+      .whenPressed(new ParallelCommandGroup(
+        new InstantCommand(shooter::runKicker, intake),
+        new InstantCommand(hopper::runHopper, hopper)))
+      .whenReleased(new ParallelCommandGroup(
+        new InstantCommand(shooter::stopKicker, intake),
+        new InstantCommand(hopper::stop, hopper)));
 
     // reverse hopper
-    new JoystickButton(driveController, Constants.kHopper.REVERSE_HOPPER)
-      .whenPressed(new InstantCommand(hopper::runBackward, hopper))
-      .whenReleased(new InstantCommand(hopper::stop, hopper));
+    new JoystickButton(driveController, Constants.kOI.REVERSE_HOPPER)
+      .whenPressed(new ParallelCommandGroup(
+        new InstantCommand(intake::reverseIntake, intake),
+        new InstantCommand(hopper::reverseHopper, hopper)))
+      .whenReleased(new ParallelCommandGroup(
+        new InstantCommand(intake::stop, intake),
+        new InstantCommand(hopper::stop, hopper)));
 
     // Actuate Intake
-    new JoystickButton(driveController, Constants.kIntake.ACTUATE_INTAKE)
-      .whenPressed(new InstantCommand(intake::actuateIntake, intake));
-
-    // Retract Intake
-    new JoystickButton(driveController, Constants.kIntake.RETRACT_INTAKE)
-      .whenPressed(new InstantCommand(intake::retractIntake, intake));
+    new JoystickButton(driveController, Constants.kOI.TOGGLE_INTAKE)
+      .whenPressed(new InstantCommand(intake::toggleIntake, intake));
       
     // Run Intake
-    new JoystickButton(driveController, Constants.kIntake.RUN_INTAKE)
-      .whenPressed(new InstantCommand(intake::startIntake, intake))
-      .whenReleased(new InstantCommand(intake::stopIntake, intake));
+    new JoystickButton(driveController, Constants.kOI.RUN_INTAKE)
+      .whenPressed(new ParallelCommandGroup(
+        new InstantCommand(intake::runIntake, intake),
+        new InstantCommand(hopper::runHopper, hopper)))
+      .whenReleased(new ParallelCommandGroup(
+        new InstantCommand(intake::stop, intake),
+        new InstantCommand(hopper::stop, hopper)));
 
     // Reverse Intake
-    new JoystickButton(driveController, Constants.kIntake.REVERSE_INTAKE)
-      .whenPressed(new InstantCommand(intake::startReverse, intake))
-      .whenReleased(new InstantCommand(intake::stopIntake, intake));
+    new JoystickButton(driveController, Constants.kOI.REVERSE_INTAKE)
+      .whenPressed(new InstantCommand(intake::reverseIntake, intake))
+      .whenReleased(new InstantCommand(intake::stop, intake));
+
+    // Run Shooter
+    new JoystickButton(driveController, Constants.kOI.RUN_SHOOTER)
+      .whenPressed(new InstantCommand(shooter::runShooter, shooter))
+      .whenReleased(new InstantCommand(shooter::stopShooter));
 
     drivetrain.setDefaultCommand(new RunCommand(() -> drivetrain.curveDrive(OI.getTriggers(driveController),
       OI.getLeftStick(driveController), driveController.getXButton()), drivetrain));
